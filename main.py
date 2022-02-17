1ec7b2c8accd5a3ff5fa7c9a1d90d8046b02a0c2
Alex Sutter <46454849+sutter12@users.noreply.github.com>
2022-02-17 18:08:11 +0000

    Added File logging

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
 # Author: Alexander Sutter
 # Date: 2/16/22
-# Last Modified: 2/16/22
+# Last Modified: 2/17/22
+
+def openLogFile(fileName="fileReadingWriting_Base_Default.txt"):
+  try:
+    return open(fileName, "a")
+  except:
+    print("error opening file")
+
+def writeLog(logItem):
+  logFile = openLogFile("fileReadingWriting_Base_Default.txt")
+  logFile.write(logItem + "\n")
+  logFile.close()
+
+def log(logItem):
+  # print("log item: " +str(logItem))
+  writeLog("log item: " +str(logItem))
+
+def logError(logItem):
+  # print("Error: " +str(logItem))
+  writeLog("Error: " +str(logItem))
 
 def main():
   print("Hello World")
+  log("Hello World")
 
 main()
